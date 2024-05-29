568e6ab9dadfceb98a99c5b076abca3bf208a169
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 09:36:07 -0600

    Add cors and fastify auth plugin

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import fp from 'fastify-plugin'
+import cors from '@fastify/cors'
 import { MongoService } from './service/index.js'
 import { Shop as ShopController } from './controller/index.js'
 import { Shop as ShopRoute } from './route/index.js'
@@ -15,6 +16,26 @@ async function decorateFastifyInstance (fastify, options, done) {
   fastify.decorate('shop', new ShopController())
 }
 
+async function autheticateInstance (fastify, opts) {
+  fastify.register(import('@fastify/jwt'), {
+    secret: process.env.JWT_SECRET
+  })
+
+  fastify.decorate('authenticate', async function (request, reply) {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  })
+}
+
 app
+  .register(cors, {
+    origin: true,
+    credentials: true,
+    allowedHeaders: 'Authorization, Origin, X-Requested-With, Content-Type, Accept'
+  })
+  .register(fp(autheticateInstance))
   .register(fp(decorateFastifyInstance))
   .register(ShopRoute, { prefix: '/api/shop' })
