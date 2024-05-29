6a648832092fb3bf5a23b11ad7fd186d9bdbc68b
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 08:22:08 -0600

    Add a store to manage shops

---
+++ frontend/src/stores/shop.js
@@ -0,0 +1,86 @@
+import { ref, watchEffect } from 'vue'
+import { defineStore } from 'pinia'
+import ApiServices from '@/service/ApiService'
+import axios from '@/boot/axios'
+
+export const useShopStore = defineStore('shop', () => {
+  const shopApi = new ApiServices({ 
+    path: 'shop',
+    axios, 
+  })
+  const shop = ref(null)
+  const shops = ref([])
+  const isLoading = ref(false)
+  const error = ref(null)
+
+  async function getShops() {
+    try {
+      isLoading.value = true
+      const response = await shopApi.getAll()
+      shops.value = response.data.data
+    } catch (error) {
+      error.value = 'Error fetching shops'
+    } finally {
+      isLoading.value = true
+    }
+  }
+
+  async function updateShop(newShop) {
+    try {
+      isLoading.value = true
+      const response = await shopApi.update({ id: newShop._id, shop: newShop })
+      console.log(response.data)
+      shops.value = shops.value.map(s => {
+        if (s._id === newShop._id) {
+          s = response.data
+        }
+        return s
+      })
+    } catch (error) {
+      console.log(error)
+      error.value = 'Error updating shop'
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function addShop(newShop) {
+    try {
+      isLoading.value = true
+      const response = await shopApi.create(newShop)
+      shops.value.push(response.data)
+    } catch (error) {
+      error.value = 'Error adding a shop'
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function deleteShop(id) {
+    try {
+      isLoading.value = true
+      await shopApi.delete(id)
+      shops.value = shops.value.filter(s => s._id !== id)
+    } catch (error) {
+      error.value = 'Error deliting a shop'
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  function selectShop(id) {
+    shop.value = shops.value.find(s => s._id === id)
+  }
+
+  getShops()
+
+  return {
+    shop,
+    shops,
+    getShops,
+    addShop,
+    updateShop,
+    deleteShop,
+    selectShop
+  }
+})
