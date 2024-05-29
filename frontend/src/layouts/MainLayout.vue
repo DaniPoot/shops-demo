455a2233829136cadf6c0f1582764b90700de994
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 08:21:31 -0600

    Add a common layout to our page

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,125 @@
+<script setup>
+import {
+  Home,
+  MapPinned,
+  UserCog,
+  PanelLeft,
+  Settings,
+  Package2
+} from 'lucide-vue-next'
+
+import { Button } from '@/components/ui/button'
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+</script>
+
+<template>
+  <div class="flex min-h-screen w-full flex-col bg-muted/40">
+    <aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <nav class="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <a
+          href="#"
+          class="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <Package2 class="h-4 w-4 transition-all group-hover:scale-110" />
+          <span class="sr-only">Acme Inc</span>
+        </a>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <RouterLink 
+                :to="{ name: 'locations'}"
+                class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              >
+                <MapPinned class="h-5 w-5" />
+                <span class="sr-only">Locations</span>
+              </RouterLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              Locations
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+            <RouterLink 
+                :to="{ name: 'admin'}"
+                class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              >
+                <UserCog class="h-5 w-5" />
+                <span class="sr-only">Admin</span>
+              </RouterLink>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              Admin
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </nav>
+      <nav class="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <a
+                href="#"
+                class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              >
+                <Settings class="h-5 w-5" />
+                <span class="sr-only">Settings</span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              Settings
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </nav>
+    </aside>
+    <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <header class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <Sheet>
+          <SheetTrigger as-child>
+            <Button size="icon" variant="outline" class="sm:hidden">
+              <PanelLeft class="h-5 w-5" />
+              <span class="sr-only">Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" class="sm:max-w-xs">
+            <nav class="grid gap-6 text-lg font-medium">
+              <a
+                href="#"
+                class="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              >
+                <Package2 class="h-5 w-5 transition-all group-hover:scale-110" />
+                <span class="sr-only">Acme Inc</span>
+              </a>
+              <a
+                href="#"
+                class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Home class="h-5 w-5" />
+                Dashboard
+              </a>
+              <a
+                href="#"
+                class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <ShoppingCart class="h-5 w-5" />
+                Orders
+              </a>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </header>
+      <slot></slot>
+    </div>
+  </div>
+</template>
