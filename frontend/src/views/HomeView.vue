a4b891bff21d3e4f45177d4fe04251ccc6005a92
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 15:18:01 -0600

    Add some classes to better ui

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -12,13 +12,15 @@ retrieveSession()
 
 
 <template>
-  <h1>Full Stack Demo App</h1>
-  <p>This site is using: </p>
-  <ul>
-    <li>Vue ecosystem</li>
-    <li>Mapbox</li>
-    <li>Shadcn/Vue</li>
-  </ul>
+  <div class="text-center text-2xl">
+    <h1 class="text-7xl font-semibold m-3">Full Stack Demo App</h1>
+    <p>This site is using: </p>
+    <ul class="text-xl list-disc">
+      <li>Vue ecosystem</li>
+      <li>Mapbox</li>
+      <li>Shadcn/Vue</li>
+    </ul>
+  </div>
 </template>
 
 <style></style>
