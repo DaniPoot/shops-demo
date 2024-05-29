3a75a7b09e543b564b76bc74357030a8acae2a2f
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 15:17:42 -0600

    Remove unused elements and add correct links

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -4,8 +4,6 @@ import {
   MapPinned,
   UserCog,
   PanelLeft,
-  Settings,
-  Package2
 } from 'lucide-vue-next'
 
 import { Button } from '@/components/ui/button'
@@ -24,13 +22,13 @@ import {
   <div class="flex min-h-screen w-full flex-col bg-muted/40">
     <aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav class="flex flex-col items-center gap-4 px-2 sm:py-5">
-        <a
-          href="#"
+        <RouterLink
+          to="/"
           class="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <Package2 class="h-4 w-4 transition-all group-hover:scale-110" />
-          <span class="sr-only">Acme Inc</span>
-        </a>
+          <Home class="h-4 w-4 transition-all group-hover:scale-110" />
+          <span class="sr-only">Map App</span>
+        </RouterLink>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger as-child>
@@ -64,24 +62,6 @@ import {
           </Tooltip>
         </TooltipProvider>
       </nav>
-      <nav class="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger as-child>
-              <a
-                href="#"
-                class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Settings class="h-5 w-5" />
-                <span class="sr-only">Settings</span>
-              </a>
-            </TooltipTrigger>
-            <TooltipContent side="right">
-              Settings
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </nav>
     </aside>
     <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
       <header class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
@@ -94,27 +74,27 @@ import {
           </SheetTrigger>
           <SheetContent side="left" class="sm:max-w-xs">
             <nav class="grid gap-6 text-lg font-medium">
-              <a
-                href="#"
+              <RouterLink
+                to="/"
                 class="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
               >
-                <Package2 class="h-5 w-5 transition-all group-hover:scale-110" />
+                <Home class="h-5 w-5 transition-all group-hover:scale-110" />
                 <span class="sr-only">Acme Inc</span>
-              </a>
-              <a
-                href="#"
+              </RouterLink>
+              <RouterLink
+                :to="{ name: 'locations'}"
                 class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
               >
-                <Home class="h-5 w-5" />
-                Dashboard
-              </a>
-              <a
-                href="#"
+                <MapPinned class="h-5 w-5" />
+                Locations
+              </RouterLink>
+              <RouterLink
+                to="{ name: 'admin'}"
                 class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
               >
-                <ShoppingCart class="h-5 w-5" />
-                Orders
-              </a>
+                <UserCog class="h-5 w-5" />
+                Admin
+              </RouterLink>
             </nav>
           </SheetContent>
         </Sheet>
