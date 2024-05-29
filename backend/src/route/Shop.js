9574f33cdac6e2c19b906f4bc1735a796b54222f
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 09:36:38 -0600

    Add prehandler to block routes without authentication

--- backend/src/route/Shop.js
+++ backend/src/route/Shop.js
@@ -9,27 +9,39 @@ export default async function (fastify, _opts) {
       })
 
     fastify.post('/create',
-      { schema: insertShopSchema },
+      {
+        schema: insertShopSchema,
+        preHandler: [fastify.authenticate]
+      },
       async function (request, reply) {
         return this.shop.create(request.body)
       })
 
     fastify.get('/:storeId',
-      { schema: getByIdShema },
+      {
+        schema: getByIdShema,
+        preHandler: [fastify.authenticate]
+      },
       async function (request, reply) {
         const { storeId } = request.params
         return this.shop.getById(storeId)
       })
 
     fastify.put('/:storeId',
-      { schema: updateShopSchema },
+      {
+        schema: updateShopSchema,
+        preHandler: [fastify.authenticate]
+      },
       async function (request, reply) {
         const { storeId } = request.params
         return this.shop.update({ id: storeId, shop: request.body })
       })
 
     fastify.delete('/:storeId',
-      { schema: deleteShopSchema },
+      {
+        schema: deleteShopSchema,
+        preHandler: [fastify.authenticate]
+      },
       async function (request, reply) {
         const { storeId } = request.params
         return this.shop.delete(storeId)
