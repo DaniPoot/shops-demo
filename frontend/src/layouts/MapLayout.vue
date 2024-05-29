4af8939c44f740d618517c9c35d9e253729ee7d2
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 08:21:50 -0600

    Add a common layout to manage our map pages

---
+++ frontend/src/layouts/MapLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+</script>
+
+<template>
+  <div class="grid h-screen w-full">
+    <div class="flex flex-col">
+      <main class="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+        <div>
+          <slot name="aside"></slot>
+        </div>
+        <div class="lg:col-span-2" >
+          <div class="flex-1" />
+          <slot></slot>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
