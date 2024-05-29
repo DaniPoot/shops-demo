37c3592c8e1f11fe89a868402ec7b9201ab69dab
DaniPoot <danielpoot.uc2@gmail.com>
2024-05-29 08:17:50 -0600

    Add a api service to connect to our api

---
+++ frontend/src/service/ApiService.js
@@ -0,0 +1,30 @@
+export default class ApiService {
+  constructor ({
+    path,
+    axios
+  }) {
+    this.path = path
+    this.axios = axios
+  }
+
+  getAll () {
+    return this.axios.get(this.path)
+  }
+
+  getById (id) {
+    return this.axios.get(`${this.path}/${id}`)
+  }
+
+  create (shop) {
+    console.log({ shop }, 'api')
+    return this.axios.post(`${this.path}/create`, shop)
+  }
+
+  update ({ id, shop }) {
+    return this.axios.put(`${this.path}/${id}`, JSON.stringify(shop))
+  }
+
+  delete (id) {
+    return this.axios.delete(`${this.path}/${id}`, {})
+  }
+}
